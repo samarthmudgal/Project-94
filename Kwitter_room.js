f751c361e054055b67358ed1d7fe315f7dd28f62
samarthmudgal <90464416+samarthmudgal@users.noreply.github.com>
2022-11-22 17:02:52 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC94NhjUuBglQGyiJqn9hkZdy1DZjjU0DE",
+    authDomain: "project-94-c0942.firebaseapp.com",
+    projectId: "project-94-c0942",
+    storageBucket: "project-94-c0942.appspot.com",
+    messagingSenderId: "829750355763",
+    appId: "1:829750355763:web:14e85515f08421e6de4046",
+    measurementId: "G-EVTRKCM7VR"
+  };
+  
